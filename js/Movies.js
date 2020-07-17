d61f1683d9dbc2e7f7fd264bcc1f62e3d40d5035
Zaki <Cloud9CPP@yandex.ru>
2020-07-17 09:54:39 +0500

    Upgrade simple code

--- js/Movies.js
+++ js/Movies.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -8,12 +10,25 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последжних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последжних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+let a, b;
+for(let i = 0; i < 1; i++) {
+    a = prompt('Один из последних просмотренных фильмов?', '');
+    b = prompt('На сколько оцените его?', '');
+    if (a === null || a.length > 50 || a === '') {
+        i -= 1;
+    }
+}
 
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+if (personalMovieDB.count === 0) {
+    alert('Ни один трейлер к фильму вас не заманит');
+} else if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
